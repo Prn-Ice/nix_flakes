c70fc1a54a75295f32692b4752d4dd56d8fb90fb
Prn-Ice <stormprince77@gmail.com>
2025-01-07 17:03:53 +0100

    fix: update android sdk code

--- flutter_flake/flake.nix
+++ flutter_flake/flake.nix
@@ -4,31 +4,52 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            # android_sdk.accept_license = true;
-            # allowUnfree = true;
-          };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
         };
-        # androidSdk = pkgs.androidenv.androidPkgs_9_0.androidsdk;
-      in
-      {
-        devShell =
-          with pkgs; mkShell rec
-            {
-              # ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
-              buildInputs = [
-                # flutter
-                jdk
-              ] ++ (if stdenv.isDarwin then [
+      };
+      buildToolsVersion = "34.0.0";
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [buildToolsVersion "28.0.3"];
+        platformVersions = ["34" "28"];
+        abiVersions = ["armeabi-v7a" "arm64-v8a"];
+      };
+      androidSdk = androidComposition.androidsdk;
+    in {
+      devShell = with pkgs;
+        mkShell {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          buildInputs =
+            [
+              flutter
+              jdk
+              lcov
+            ]
+            ++ (
+              if stdenv.isDarwin
+              then [
                 cocoapods
-              ] else [ ]);
-            } // (if !stdenv.isDarwin then {
-            CHROME_EXECUTABLE = "google-chrome-stable";
-          } else { });
-      });
+              ]
+              else [
+                androidSdk
+              ]
+            );
+
+          shellHook =
+            if !stdenv.isDarwin
+            then ''
+              export CHROME_EXECUTABLE="google-chrome-stable"
+            ''
+            else "";
+        };
+    });
 }
