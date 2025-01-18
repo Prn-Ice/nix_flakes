73ef3c53be803bde1e311329e99894956f51daee
Prn-Ice <stormprince77@gmail.com>
2025-01-18 12:25:21 +0100

    fix: add even more gstreamer

--- flutter_flake/flake.nix
+++ flutter_flake/flake.nix
@@ -52,6 +52,22 @@
               gst_all_1.gst-plugins-bad
               gst_all_1.gst-plugins-ugly
               gst_all_1.gst-libav
+              pkg-config
+              glib
+              gtk3
+              xorg.libX11
+              xorg.libXcomposite
+              xorg.libXcursor
+              xorg.libXdamage
+              xorg.libXext
+              xorg.libXfixes
+              xorg.libXi
+              xorg.libXrandr
+              xorg.libXrender
+              xorg.libXtst
+              xorg.libxcb
+              libGL
+              libGLU
             ]
           );
 
