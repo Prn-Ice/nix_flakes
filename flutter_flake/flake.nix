5dbcbe623744c08eb868590595e78c83157d35a2
Prn-Ice <stormprince77@gmail.com>
2025-06-01 20:01:20 +0100

    feat: nix-ld experiment

--- flutter_flake/flake.nix
+++ flutter_flake/flake.nix
@@ -16,23 +16,73 @@
           pkgs = import nixpkgs {
             inherit system;
             config = {
-              android_sdk.accept_license = true;
+              # android_sdk.accept_license = true;
               allowUnfree = true;
             };
           };
         });
   in {
     devShells = forEachSystem ({pkgs}: let
-      buildToolsVersion = "34.0.0";
-      androidComposition = pkgs.androidenv.composeAndroidPackages {
-        buildToolsVersions = [buildToolsVersion "28.0.3"];
-        platformVersions = ["34" "28"];
-        abiVersions = ["armeabi-v7a" "arm64-v8a"];
-      };
-      androidSdk = androidComposition.androidsdk;
+      # buildToolsVersion = "34.0.0";
+      # androidComposition = pkgs.androidenv.composeAndroidPackages {
+      #   buildToolsVersions = [buildToolsVersion "28.0.3"];
+      #   platformVersions = ["34" "33" "31" "28"];
+      #   abiVersions = ["armeabi-v7a" "arm64-v8a"];
+      #   includeEmulator = true;
+      # };
+      # androidSdk = androidComposition.androidsdk;
     in {
       default = pkgs.mkShell {
-        ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+        ANDROID_SDK_ROOT = "/home/prnice/Android/Sdk";
+        ANDROID_HOME = "/home/prnice/Android/Sdk";
+        # ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+        # override the aapt2 that gradle uses with the nix-shipped version
+        # GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/${buildToolsVersion}/aapt2";
+
+        NIX_LD_LIBRARY_PATH = with pkgs;
+          "/home/prnice/Android/Sdk/emulator/lib64/:" +
+          "/home/prnice/Android/Sdk/emulator/lib64/qt/lib/:" +
+          lib.makeLibraryPath [
+            # For emulator
+            gperftools.out
+            libpulseaudio.out
+            xorg.libX11.out
+
+            # For qemu
+            nss
+            nspr
+            libpng
+            expat
+            libdrm
+            xorg.libxcb
+            libjpeg
+            fontconfig
+            freetype
+            xorg.libXi
+            xorg.libXext
+            xorg.libxkbfile
+            pcre2
+            libsndfile
+            xorg.libXau
+            xorg.libXdmcp
+            flac
+            libvorbis
+            libopus
+            libogg
+            mpg123
+            lame
+            libcap
+            libuuid
+            libbsd
+            xorg.libXcursor
+
+            # Others
+            stdenv.cc.cc.lib
+            zlib
+            libGL
+          ];
+        NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+
         buildInputs = with pkgs;
           [
             zulu17
@@ -45,7 +95,8 @@
             ]
             else [
               flutter
-              androidSdk
+              # androidSdk
+              ruby
               gst_all_1.gstreamer
               gst_all_1.gst-plugins-base
               gst_all_1.gst-plugins-good
